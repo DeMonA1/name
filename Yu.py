4810cbacb8f61c018cb60682c5ec05036a31ebd8
Demon <you@awe.com>
2023-03-01 21:32:58 +0300

    'sad'

---
+++ Yu.py
@@ -0,0 +1,10 @@
+# Орёл и решка
+import random
+res_1 = random.randint (1, 100)
+res_2 = int
+tries = 1
+while tries != 100:
+    tries += 1 
+    res_2 = tries - res_1
+print ('Количество решек', res_1, 'количество орлов', res_2)
+input ('\n\nНажмите кнопку')
